6b48c90f573b898cec5235ad908bf183fa5a081e
stanleyshuang <stanleyshuang@qnap.com>
2021-07-10 15:03:52 +0800

    init env

--- app/main.py
+++ app/main.py
@@ -11,18 +11,39 @@ from pkg.cvecrawler.cveproject import cveproject
 from pkg.cvecrawler.cve_json import is_cve_json_filename
 from pkg.util.util_file import create_folder, get_name_list_of_files
 
+g_apphome = ''
+g_downloads = ''
+g_inputs = ''
+
 def usage():
     print('USAGE:    python main.py cmd')
     print('--')
     print('cmd:      --test for unit test')
     print('          --crawler for default')
     quit()
+
+def env_init():
+    g_apphome = os.environ.get('apphome')
+
+    ### Create g_downloads
+    g_downloads = g_apphome + '/downloads'
+    create_folder(g_downloads)
+
+    ### Enumerate g_inputs
+    g_inputs = g_apphome + '/inputs'
+    filelist = get_name_list_of_files(g_inputs)
+
+    for file in filelist:
+        filename, file_extension = os.path.splitext(file)
+        print('{filename} {file_extension}'.format(filename=filename, file_extension=file_extension))
     
+###################################################
+### main program begin
 if len(sys.argv) == 1:
     usage()
     quit()
 
-### get argv[1] as input
+# get argv[1] as input
 cmd = 'crawler'
 for idx in range(1, len(sys.argv)):
     if sys.argv[idx] in ['--test']:
@@ -32,25 +53,12 @@ for idx in range(1, len(sys.argv)):
     else:
         pass
 
-### the main program
-# Get environment variables
-
-### Create downloads folder
-apphome = os.environ.get('apphome')
-downloads = apphome + '/downloads'
-create_folder(downloads)
-
-### Enumerate inputs folder
-inputs = apphome + '/inputs'
-filelist = get_name_list_of_files(inputs)
-for file in filelist:
-    filename, file_extension = os.path.splitext(file)
-    print('{filename} {file_extension}'.format(filename=filename, file_extension=file_extension))
-
+# parse command and execute
 if cmd=='test':
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 else: 
+    env_init()
     cveproject = cveproject()
     cveproject.dump()
---
+++ app/shell/sync.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+base_dir=$(dirname "$0")
+cvelist_home=$HOME/github/stanleyshuang/cvelist
+cd $cvelist_home
+
+### 1. Ensure your fork is up to date, especially prior to creating a new 
+###    branch (every time you create a new branch). The command for this are:
+git fetch upstream
+git checkout master
+git merge upstream/master
+
+### 2. Optionally push any updates from the upstream CVEProject/cvelist 
+###    master back to you fork on Github.com:
+git push
